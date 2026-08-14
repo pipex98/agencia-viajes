1439355165c7e3d2529711e6c6b0091286c79ed3
Felipe Vasquez Gomez <felipe.dev1998@gmail.com>
2026-08-14 15:39:53 -0500

    add red phase test

---
+++ AgenciaViajes.Tests/AgenciaViajesApiFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AgenciaViajes.API.Tests
+{
+    public class AgenciaViajesApiFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.PostConfigure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.TokenValidationParameters.ValidateIssuerSigningKey = false;
+                    options.TokenValidationParameters.ValidateIssuer = false;
+                    options.TokenValidationParameters.ValidateAudience = false;
+                    options.TokenValidationParameters.ValidateLifetime = false;
+                    options.TokenValidationParameters.RequireSignedTokens = false;
+                });
+            });
+        }
+    }
+}
---
+++ AgenciaViajes.Tests/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace AgenciaViajes.API.Tests
+{
+    public static class Utils
+    {
+        public static string GetJwtToken()
+        {
+            var token = new JwtSecurityToken(
+                issuer: "test",
+                audience: "test",
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: null,
+                claims:
+                [
+                    new Claim(JwtRegisteredClaimNames.Sub, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Email, "test@example.com")
+                ]
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
